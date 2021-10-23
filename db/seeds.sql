d5e6054d0278179adfbf582bcb40f81e33e01a7d
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-10-23 18:22:27 -0400

    seed SQL

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+use employees;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Mehreen', 'Cobb', 1, NULL),
+    ('Raj', 'Deleon', 2, 1),
+    ('Janet', 'Stewart', 3, NULL),
+    ('Laith', 'Mcarthur', 4, 3),
+    ('Kairon', 'Clegg', 5, NULL),
+    ('Blaine', 'Kavanagh', 6, 5),
+    ('Eddison', 'Burgess', 7, NULL),
+    ('Connar', 'Mullen', 8, 7);
